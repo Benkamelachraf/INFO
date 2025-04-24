54774ac1c91d17947e639211501b21dc1c884c9c
Sohan <yahyaessyahi0@gmail.com>
2025-04-24 04:12:59 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,9 +1,42 @@
 function confirmDelete(cin) {
 
-    if (confirm("Voulez-vous vraiment supprimer CIN: " + cin + " ?")) {
+  if (confirm("Voulez-vous vraiment supprimer CIN: " + cin + " ?")) {
 
-        window.location.href = "action.php?delete=" + cin;
+    window.location.href = "action.php?delete=" + cin;
+  }
+}
 
-    }
+function validateForm() {
+  const nom = document.querySelector('[name="nom"]').value;
+  const prenom = document.querySelector('[name="prenom"]').value;
+  const adresse = document.querySelector('[name="adresse"]').value;
 
+  if (nom.length < 2) {
+      alert('Nom doit avoir au moins 3 caractères.');
+      return false;
+  }
+  if (nom.length > 50) {
+      alert('Nom ne peut pas dépasser 50 caractères.');
+      return false;
+  }
+
+  if (prenom.length < 2) {
+      alert('Prénom doit avoir au moins 3 caractères.');
+      return false;
+  }
+  if (prenom.length > 50) {
+      alert('Prénom ne peut pas dépasser 50 caractères.');
+      return false;
+  }
+
+  if (adresse.length < 5) {
+      alert('Adresse doit avoir au moins 5 caractères.');
+      return false;
+  }
+  if (adresse.length > 50) {
+      alert('Adresse ne peut pas dépasser 100 caractères.');
+      return false;
+  }
+
+  return true;
 }
