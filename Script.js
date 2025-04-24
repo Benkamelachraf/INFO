e62a508aad4bb838a53c0981f88f1d7eb579dc6d
Sohan <yahyaessyahi0@gmail.com>
2025-04-24 09:56:49 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -5,6 +5,7 @@ function confirmDelete(cin) {
     window.location.href = "action.php?delete=" + cin;
   }
 }//action.php?delete=7352863
+// en généralement Ouvre la page action.php et envoie-lui le cin pour suppression.
 
 function validateForm() {
   //function pour respecter les longueurs max et min
